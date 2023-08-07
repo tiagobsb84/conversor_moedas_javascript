d51b1bdbccff720655e16f8d0d39d24b45674d1e
Tiago Silva <tiagobsb84@gmail.com>
2023-08-07 20:53:09 -0300

    fazendo a conversao dos valores dinamicamente

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 const buttonConvertion = document.querySelector(".button-convertion");
 const inputValue = document.querySelector(".input-value");
 const firstValueCurrency = document.querySelector(".first-value");
+const selectionCurrency = document.querySelector(".select-currency");
+const secondValueCurrency = document.querySelector(".second-value");
 
-function pegandoValor() {
+function conversaoValores() {
 
-    firstValueCurrency.innerHTML = inputValue.value;
+    firstValueCurrency.innerHTML = `R$ ${inputValue.value}`;
+
+    const currencyDolar = 5.2;
+    const currencyEuro = 6.1;
+    const currencyLibra = 6.2;
+    const currencyBitcoin = 870.243;
+
+    if(selectionCurrency.value == "dolar") {
+        secondValueCurrency.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue.value / currencyDolar);
+    }
+
+    if(selectionCurrency.value == "euro") {
+        secondValueCurrency.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue.value / currencyEuro);
+    }
+
+    if(selectionCurrency.value == "libra") {
+        secondValueCurrency.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputValue.value / currencyLibra);
+    }
+
+    if(selectionCurrency.value == "bitcoin") {
+        secondValueCurrency.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputValue.value / currencyBitcoin);
+    }
 }
 
-buttonConvertion.addEventListener("click", pegandoValor);
+buttonConvertion.addEventListener("click", conversaoValores);
